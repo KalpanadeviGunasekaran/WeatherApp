e7738294f06359e669fa6881fd0e9e1b1cd33cbf
KalpanadeviGunasekaran <128279103+KalpanadeviGunasekaran@users.noreply.github.com>
2024-07-01 22:31:46 +0530

    Add files via upload

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '1ea9ec0ea3f947c6f7c96bf6fc93a848';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
